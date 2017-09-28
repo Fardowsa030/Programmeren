6b1b829c951c349f482e6d42aee1bae6f3a3b46b
Fardowsa <Firdowsa_03@live.nl>
2017-09-28 11:29:01 +0200

    alles naar github

--- Practice Exercises/Les 7/Practice Exercise 7_5.py
+++ Practice Exercises/Les 7/Practice Exercise 7_5.py
@@ -1,2 +1,18 @@
 
-
+naam = input('enter name: ')
+while naam != '':
+    list = []
+    dict = {}
+    naam = input('enter name: ')
+    list.append(naam)
+for item in list:
+    if item in dict:
+        dict[item] += 1
+    else:
+        dict[item] = 1
+print(dict)
+for key in dict:
+    if dict[key] == 1:
+        print('Er is {} student met de naam {}'.format(dict[key], key))
+    else:
+            print('Er zijn {} studenten met de naam {}'.format(dict[key], key))
